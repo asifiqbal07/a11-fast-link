07a67874fb541bb80c86d9c38e5a46a081e8df6c
asifiqbal07 <as.asif.07@gmail.com>
2022-11-08 23:58:53 +0600

    Package Details Completed

--- src/Pages/Home/HomePackages/HomePackage.js
+++ src/Pages/Home/HomePackages/HomePackage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaArrowRight } from "react-icons/fa";
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 
 const HomePackage = ({ homePack }) => {
     const { title, image_url, details, _id, price } = homePack;
@@ -8,11 +10,15 @@ const HomePackage = ({ homePack }) => {
     return (
         <div>
             <div className="card card-compact  bg-base-100 shadow-xl p-4  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 rounded-none">
-                <figure><img className='h-[250px] w-full' src={image_url} alt="Shoes" /></figure>
+                <figure><PhotoProvider>
+                    <PhotoView src={image_url}>
+                        <img className='h-[250px] w-full' src={image_url} alt=""  />
+                    </PhotoView>
+                    </PhotoProvider></figure>
                 <div className="card-body">
                     <div className='flex items-center justify-between'>
-                    <h2 className="card-title">{title}</h2>
-                    <h2 className="card-title t">{price}tk</h2>
+                        <h2 className="card-title">{title}</h2>
+                        <h2 className="card-title t">{price}tk</h2>
                     </div>
                     <p>{details.slice(0, 95) + '...'}</p>
                     <Link to={`/packages/${_id}`}><span className='flex items-center text-[#3078fb] justify-center font-semibold mt-3 hover:text-xl'>Check Details <FaArrowRight className='ml-5 mt-1' /></span></Link>
--- src/Pages/Packages/PackageDetails/PackageDetails.js
+++ src/Pages/Packages/PackageDetails/PackageDetails.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+import { FaStar } from 'react-icons/fa';
 
 const PackageDetails = () => {
     const packageDetails = useLoaderData();
+    const { title, price, image_url, details, speed, rating, features} = packageDetails;
     console.log(packageDetails);
     return (
-        <div>
-            PackageDetails
+        <div className='mx-6 lg:mx-0 mb-20 mt-10'>
+            <div className='lg:flex items-center justify-between mb-6'>
+                <div className='w-1/2'>
+                    <figure><PhotoProvider>
+                        <PhotoView src={image_url}>
+                            <img className='' src={image_url} alt="" />
+                        </PhotoView>
+                    </PhotoProvider></figure>
+                </div>
+                <div className='w-1/2'>
+                    <h2 className="text-center text-4xl font-bold mb-10">Package <span className='text-[#3078fb]'>{title}</span> </h2>
+                    <div className="card card-compact  bg-base-100 shadow-lg p-4  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 rounded-none w-4/5 mx-auto">
+                        <div className="card-body">
+                            <div className='flex items-center justify-between'>
+                                <h2 className="card-title ">{price}tk</h2>
+                                <h2 className="card-title ">{speed}Mbps</h2>
+                            </div>
+                            <p className='mb-4'>{details}</p>
+                            <hr />
+                            <div className='flex items-center justify-between mt-4'>
+                                <div >
+                                    <h1>Features</h1>
+                                </div>
+                                <span className='flex items-center'>{rating}<FaStar></FaStar></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
